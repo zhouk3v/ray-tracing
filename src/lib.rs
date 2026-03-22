e486e62bc68addc69a322c836c2b27a9549de57f
Kevin Zhou <zhoukev@hotmail.com>
2026-03-22 18:15:30 -0400

    3.1. Color Utility Functions

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::fmt;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Vec3 {
-    e: [f64; 3],
+    pub e: [f64; 3],
 }
 
 impl Vec3 {
@@ -160,3 +160,40 @@ pub fn unit_vector(v: Vec3) -> Vec3 {
 }
 
 pub type Point3 = Vec3;
+
+pub type Color = Vec3;
+
+/*
+* Switchable stream version
+*/
+
+// use std::io::{self, Write};
+
+// pub fn write_color(pixel_color: &Color) -> io::Result<()> {
+//     let r = pixel_color.x();
+//     let g = pixel_color.y();
+//     let b = pixel_color.z();
+
+//     let rbyte = (255.999 * r) as i64;
+//     let gbyte = (255.999 * g) as i64;
+//     let bbyte = (255.99 * b) as i64;
+
+//     writeln!(out, "{rbyte} {gbyte} {bbyte}")?;
+//     Ok(())
+// }
+
+/*
+* println! version
+*/
+
+pub fn write_color(pixel_color: &Color) {
+    let r = pixel_color.x();
+    let g = pixel_color.y();
+    let b = pixel_color.z();
+
+    let rbyte = (255.999 * r) as i64;
+    let gbyte = (255.999 * g) as i64;
+    let bbyte = (255.99 * b) as i64;
+
+    println!("{rbyte} {gbyte} {bbyte}")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use ray_tracing::{write_color, Color};
+
 fn main() {
     // Image
 
@@ -12,16 +14,16 @@ fn main() {
         let remaining = image_height - j;
         eprintln!("Scanlines remaining {remaining}");
         for i in 0..image_width {
-            let r = i as f64 / (image_width - 1) as f64;
-            let g = j as f64 / (image_height - 1) as f64;
-            let b: f64 = 0.0;
+            let pixel_color = Color {
+                e: [
+                    i as f64 / (image_width - 1) as f64,
+                    j as f64 / (image_height - 1) as f64,
+                    0.0,
+                ],
+            };
 
-            let ir = (255.999 * r) as i64;
-            let ig = (255.999 * g) as i64;
-            let ib = (255.99 * b) as i64;
-
-            println!("{ir} {ig} {ib}")
+            write_color(&pixel_color);
         }
     }
-    eprintln!("Done.")
+    eprintln!("Done.");
 }
