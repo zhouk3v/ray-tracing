83d313ebbe990bc45b93320041e523edfb5ecff6
DESKTOP-FPSVATK\zhouk <zhoukev@hotmail.com>
2026-03-22 14:51:03 -0400

    2.1 to 2.2

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    // Image
+
+    let image_width = 256;
+    let image_height = 256;
+
+    // Render
+    println!("P3");
+    println!("{image_width} {image_height}");
+    println!("255");
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b: f64 = 0.0;
+
+            let ir = (255.999 * r) as i64;
+            let ig = (255.999 * g) as i64;
+            let ib = (255.99 * b) as i64;
+
+            println!("{ir} {ig} {ib}")
+        }
+    }
+}
