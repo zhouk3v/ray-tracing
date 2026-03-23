0b28717f7b863d518a2418b8eb561c71bbe89693
Kevin Zhou <zhoukev@hotmail.com>
2026-03-22 21:17:02 -0400

    4.2. - Sending Rays Into the Scene

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
-use ray_tracing::{write_color, Color};
+use ray_tracing::{unit_vector, write_color, Color, Point3, Ray, Vec3};
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = unit_vector(*r.direction());
+    let a = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
     // Image
 
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+
+    let image_width = 400;
+
+    // Calculate the image height, and ensure that it's at least 1
+    let image_height = if image_width as f64 / aspect_ratio > 1.0 {
+        image_width as f64 / aspect_ratio
+    } else {
+        1.0
+    } as i32;
+
+    // Camera
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (image_width / image_height) as f64;
+    let camera_center = Point3::new(0.0, 0.0, 0.0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+
+    // Calculate the location of the upper left pixel
+    let viewport_upper_left =
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
     println!("P3");
@@ -14,14 +47,12 @@ fn main() {
         let remaining = image_height - j;
         eprintln!("Scanlines remaining {remaining}");
         for i in 0..image_width {
-            let pixel_color = Color {
-                e: [
-                    i as f64 / (image_width - 1) as f64,
-                    j as f64 / (image_height - 1) as f64,
-                    0.0,
-                ],
-            };
+            let pixel_center =
+                pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let r = Ray::new(camera_center, ray_direction);
 
+            let pixel_color = ray_color(&r);
             write_color(&pixel_color);
         }
     }
