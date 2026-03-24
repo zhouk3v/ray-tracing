d2a5a9f57e0dc7daaad55c3e86d8bdbdf45effc5
Kevin Zhou <zhoukev@hotmail.com>
2026-03-23 21:17:54 -0400

    5.2. - Creating Our First Raytraced Image

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-use ray_tracing::{unit_vector, write_color, Color, Point3, Ray, Vec3};
+use ray_tracing::{dot, unit_vector, write_color, Color, Point3, Ray, Vec3};
+
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+    let oc = *center - *r.origin();
+    let a = dot(r.direction(), r.direction());
+    let b = -2.0 * dot(r.direction(), &oc);
+    let c = dot(&oc, &oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant > 0.0 {
+        true
+    } else {
+        false
+    }
+}
 
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = unit_vector(*r.direction());
     let a = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
