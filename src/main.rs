0112561f4c9691b8c5b1506f57ab87282f657307
Kevin Zhou <zhoukev@hotmail.com>
2026-07-21 14:44:37 -0400

    10.5 - A Scene with Metal Spheres

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
-use ray_tracing::{Camera, HittableList, Point3, Sphere};
+use ray_tracing::{Camera, Color, HittableList, Lambertian, Metal, Point3, Sphere};
 
 fn main() {
     let mut world = HittableList::new();
 
-    // world.add(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
-    // world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
+    let material_ground = Lambertian::new(Color::new(0.8, 0.8, 0.0));
+    let material_center = Lambertian::new(Color::new(0.1, 0.2, 0.5));
+    let material_left = Metal::new(Color::new(0.8, 0.8, 0.8));
+    let material_right = Metal::new(Color::new(0.8, 0.6, 0.2));
+
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, -100.5, -1.0),
+        100.0,
+        Box::new(material_ground),
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, 0.0, -1.2),
+        0.5,
+        Box::new(material_center),
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.5,
+        Box::new(material_left),
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(1.0, 0.0, -1.0),
+        0.5,
+        Box::new(material_right),
+    )));
 
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400.0;
