b3504f7fded13b0f97702465ee83fe11467fd3ab
Kevin Zhou <zhoukev@hotmail.com>
2026-07-21 17:49:34 -0400

    11.5 - Modeling a Hollow Glass Sphere

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ fn main() {
 
     let material_ground = Lambertian::new(Color::new(0.8, 0.8, 0.0));
     let material_center = Lambertian::new(Color::new(0.1, 0.2, 0.5));
-    let material_left = Dielectric::new(1.00 / 1.33);
+    let material_left = Dielectric::new(1.50);
+    let material_bubble = Dielectric::new(1.00 / 1.50);
     let material_right = Metal::new(Color::new(0.8, 0.6, 0.2), 1.0);
 
     world.add(Box::new(Sphere::new(
@@ -22,6 +23,11 @@ fn main() {
         Point3::new(-1.0, 0.0, -1.0),
         0.5,
         Box::new(material_left),
+    )));
+    world.add(Box::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.4,
+        Box::new(material_bubble),
     )));
     world.add(Box::new(Sphere::new(
         Point3::new(1.0, 0.0, -1.0),
