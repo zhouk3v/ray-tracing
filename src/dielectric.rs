591dec2ec71bcf6fc532cad3f35b8dd77c18c567
Kevin Zhou <zhoukev@hotmail.com>
2026-07-21 15:26:20 -0400

    11.3 - Total Internal Reflection

--- src/dielectric.rs
+++ src/dielectric.rs
@@ -2,7 +2,7 @@ use crate::color::Color;
 use crate::hittable::HitRecord;
 use crate::material::{Material, ScatterRes};
 use crate::ray::Ray;
-use crate::vec3::{refract, unit_vector};
+use crate::vec3::{dot, reflect, refract, unit_vector};
 
 pub struct Dielectric {
     refraction_index: f64, // Refractive index in vacuum or air, or the ratio of the material's refractive index over
@@ -22,11 +22,26 @@ impl Material for Dielectric {
         } else {
             self.refraction_index
         };
+
         let unit_direction = unit_vector(*r_in.direction());
-        let refracted = refract(&unit_direction, &rec.normal, ri);
+        let cos_theta = if dot(&(-unit_direction), &rec.normal) < 1.0 {
+            dot(&(-unit_direction), &rec.normal)
+        } else {
+            1.0
+        };
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+
+        let cannot_refract = ri * sin_theta > 1.0;
+
+        let direction = if cannot_refract {
+            reflect(&unit_direction, &rec.normal)
+        } else {
+            refract(&unit_direction, &rec.normal, ri)
+        };
+
         Some(ScatterRes::new(
             Color::new(1.0, 1.0, 1.0),
-            Ray::new(rec.p, refracted),
+            Ray::new(rec.p, direction),
         ))
     }
 }
