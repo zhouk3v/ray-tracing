76062844f79dd21842bafcb2a5c7cc98192f7400
Kevin Zhou <zhoukev@hotmail.com>
2026-07-27 16:21:07 -0400

    2.1 - Introduction of SpaceTime Ray Tracing

--- src/ray.rs
+++ src/ray.rs
@@ -4,13 +4,23 @@ use crate::vec3::Vec3;
 pub struct Ray {
     orig: Point3,
     dir: Vec3,
+    tm: f64,
 }
 
 impl Ray {
-    pub fn new(origin: Point3, direction: Vec3) -> Self {
+    pub fn new(origin: Point3, direction: Vec3, time: f64) -> Self {
         Ray {
             orig: origin,
             dir: direction,
+            tm: time,
+        }
+    }
+
+    pub fn new_with_default_time(origin: Point3, direction: Vec3) -> Self {
+        Ray {
+            orig: origin,
+            dir: direction,
+            tm: 0.0,
         }
     }
 
@@ -20,6 +30,10 @@ impl Ray {
 
     pub fn direction(&self) -> &Vec3 {
         &self.dir
+    }
+
+    pub fn time(&self) -> f64 {
+        self.tm
     }
 
     pub fn at(&self, t: f64) -> Point3 {
@@ -32,6 +46,7 @@ impl Default for Ray {
         Ray {
             orig: Point3::default(),
             dir: Point3::default(),
+            tm: 0.0,
         }
     }
 }
