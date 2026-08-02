174eb049a2f7b16726a45ec58357a6a20102142c
Kevin Zhou <zhoukev@hotmail.com>
2026-08-02 18:02:42 -0400

    use enum for scenes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use ray_tracing::scenes::bouncing_spheres::bouncing_spheres;
 use ray_tracing::scenes::checkered_spheres::checkered_spheres;
 
+enum Scene {
+    BOUNCING,
+    CHECKERED,
+}
+
 fn main() {
-    let scene = 0;
+    let scene = Scene::BOUNCING;
     match scene {
-        0 => bouncing_spheres(),
-        1 => checkered_spheres(),
-        _ => bouncing_spheres(),
+        Scene::BOUNCING => bouncing_spheres(),
+        Scene::CHECKERED => checkered_spheres(),
     }
 }
