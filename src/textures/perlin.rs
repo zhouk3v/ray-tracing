be0ed4a75f7586277fb11d24ca39e82d2f1c8fa0
Kevin Zhou <zhoukev@hotmail.com>
2026-08-04 17:20:32 -0400

    5.2. - Smoothing out the Result

--- src/textures/perlin.rs
+++ src/textures/perlin.rs
@@ -23,13 +23,38 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3) -> f64 {
-        let i = (4.0 * p.x()) as i32 & 255;
-        let j = (4.0 * p.y()) as i32 & 255;
-        let k = (4.0 * p.z()) as i32 & 255;
+        // let i = (4.0 * p.x()) as i32 & 255;
+        // let j = (4.0 * p.y()) as i32 & 255;
+        // let k = (4.0 * p.z()) as i32 & 255;
 
-        let index = self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize];
+        // let index = self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize];
 
-        self.randfloat[index as usize]
+        // self.randfloat[index as usize]
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
+
+        let i = p.x().floor() as i32;
+        let j = p.y().floor() as i32;
+        let k = p.z().floor() as i32;
+
+        let mut c = [[[0.0; 2]; 2]; 2];
+
+        for (di, c1) in c.iter_mut().enumerate() {
+            for (dj, c2) in c1.iter_mut().enumerate() {
+                for (dk, c3) in c2.iter_mut().enumerate() {
+                    let perm_x_index = (i + di as i32) & 255;
+                    let perm_y_index = (j + dj as i32) & 255;
+                    let perm_z_index = (k + dk as i32) & 255;
+                    let index = self.perm_x[perm_x_index as usize]
+                        ^ self.perm_y[perm_y_index as usize]
+                        ^ self.perm_z[perm_z_index as usize];
+                    *c3 = self.randfloat[index as usize];
+                }
+            }
+        }
+
+        Self::trilinear_interp(c, u, v, w)
     }
 
     fn perlin_generate_perm() -> [i32; 256] {
@@ -46,6 +71,24 @@ impl Perlin {
             let target = rand::random_range(0..=i);
             p.swap(i, target);
         }
+    }
+
+    fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for (i, c1) in c.iter().enumerate() {
+            for (j, c2) in c1.iter().enumerate() {
+                for (k, c3) in c2.iter().enumerate() {
+                    let i_double = i as f64;
+                    let j_double = j as f64;
+                    let k_double = k as f64;
+                    accum += (i_double * u + (1.0 - i_double) * (1.0 - u))
+                        * (j_double * v + (1.0 - j_double) * (1.0 - v))
+                        * (k_double * w + (1.0 - k_double) * (1.0 - w))
+                        * c3
+                }
+            }
+        }
+        accum
     }
 }
 
