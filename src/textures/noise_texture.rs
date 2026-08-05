b394d7adadb3996ecb23a7536dd8aece9d5ac25c
Kevin Zhou <zhoukev@hotmail.com>
2026-08-05 15:21:03 -0400

    5.5 - Using Random Vectors on the Lattice Points

--- src/textures/noise_texture.rs
+++ src/textures/noise_texture.rs
@@ -21,6 +21,6 @@ impl NoiseTexture {
 impl Texture for NoiseTexture {
     fn value(&self, _u: f64, _v: f64, p: &Point3) -> Color {
         let p_new = Point3::new(p.x(), p.y(), p.z());
-        Color::new(1.0, 1.0, 1.0) * self.noise.noise(&(p_new * self.scale))
+        Color::new(1.0, 1.0, 1.0) * 0.5 * (1.0 + self.noise.noise(&(p_new * self.scale)))
     }
 }
--- src/textures/perlin.rs
+++ src/textures/perlin.rs
@@ -1,7 +1,8 @@
 use crate::primitives::point3::Point3;
+use crate::primitives::vec3::{dot, unit_vector, Vec3};
 
 pub struct Perlin {
-    randfloat: [f64; 256],
+    randvec: [Vec3; 256],
     perm_x: [i32; 256],
     perm_y: [i32; 256],
     perm_z: [i32; 256],
@@ -9,13 +10,14 @@ pub struct Perlin {
 
 impl Perlin {
     pub fn new() -> Self {
-        let mut randfloat = [0.0; 256];
-        for i in randfloat.iter_mut() {
-            *i = rand::random::<f64>();
+        let mut randvec = [Vec3::default(); 256];
+
+        for i in randvec.iter_mut() {
+            *i = unit_vector(Vec3::random_with_min_max(-1.0, 1.0));
         }
 
         Self {
-            randfloat,
+            randvec,
             perm_x: Self::perlin_generate_perm(),
             perm_y: Self::perlin_generate_perm(),
             perm_z: Self::perlin_generate_perm(),
@@ -23,26 +25,15 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3) -> f64 {
-        // let i = (4.0 * p.x()) as i32 & 255;
-        // let j = (4.0 * p.y()) as i32 & 255;
-        // let k = (4.0 * p.z()) as i32 & 255;
-
-        // let index = self.perm_x[i as usize] ^ self.perm_y[j as usize] ^ self.perm_z[k as usize];
-
-        // self.randfloat[index as usize]
-        let u_base = p.x() - p.x().floor();
-        let v_base = p.y() - p.y().floor();
-        let w_base = p.z() - p.z().floor();
-
-        let u = u_base * u_base * (3.0 - 2.0 * u_base);
-        let v = v_base * v_base * (3.0 - 2.0 * v_base);
-        let w = w_base * w_base * (3.0 - 2.0 * w_base);
+        let u = p.x() - p.x().floor();
+        let v = p.y() - p.y().floor();
+        let w = p.z() - p.z().floor();
 
         let i = p.x().floor() as i32;
         let j = p.y().floor() as i32;
         let k = p.z().floor() as i32;
 
-        let mut c = [[[0.0; 2]; 2]; 2];
+        let mut c = [[[Vec3::new(0.0, 0.0, 0.0); 2]; 2]; 2];
 
         for (di, c1) in c.iter_mut().enumerate() {
             for (dj, c2) in c1.iter_mut().enumerate() {
@@ -53,12 +44,12 @@ impl Perlin {
                     let index = self.perm_x[perm_x_index as usize]
                         ^ self.perm_y[perm_y_index as usize]
                         ^ self.perm_z[perm_z_index as usize];
-                    *c3 = self.randfloat[index as usize];
+                    *c3 = self.randvec[index as usize];
                 }
             }
         }
 
-        Self::trilinear_interp(c, u, v, w)
+        Self::perlin_interp(c, u, v, w)
     }
 
     fn perlin_generate_perm() -> [i32; 256] {
@@ -77,18 +68,23 @@ impl Perlin {
         }
     }
 
-    fn trilinear_interp(c: [[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    fn perlin_interp(c: [[[Vec3; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+        let uu = u * u * (3.0 - 2.0 * u);
+        let vv = v * v * (3.0 - 2.0 * v);
+        let ww = w * w * (3.0 - 2.0 * w);
         let mut accum = 0.0;
+
         for (i, c1) in c.iter().enumerate() {
             for (j, c2) in c1.iter().enumerate() {
                 for (k, c3) in c2.iter().enumerate() {
                     let i_double = i as f64;
                     let j_double = j as f64;
                     let k_double = k as f64;
-                    accum += (i_double * u + (1.0 - i_double) * (1.0 - u))
-                        * (j_double * v + (1.0 - j_double) * (1.0 - v))
-                        * (k_double * w + (1.0 - k_double) * (1.0 - w))
-                        * c3
+                    let weight_v = Vec3::new(u - i_double, v - j_double, w - k_double);
+                    accum += (i_double * uu + (1.0 - i_double) * (1.0 - uu))
+                        * (j_double * vv + (1.0 - j_double) * (1.0 - vv))
+                        * (k_double * ww + (1.0 - k_double) * (1.0 - ww))
+                        * dot(c3, &weight_v)
                 }
             }
         }
