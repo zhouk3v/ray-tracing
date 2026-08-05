12c472f369504eb9898c752c96e96a9c4fb5a823
Kevin Zhou <zhoukev@hotmail.com>
2026-08-05 16:06:34 -0400

    6.3 - Finding the Plane That Contains a Given Quadrilateral

--- src/hittables/quad.rs
+++ src/hittables/quad.rs
@@ -4,7 +4,7 @@ use crate::primitives::aabb::Aabb;
 use crate::primitives::interval::Interval;
 use crate::primitives::point3::Point3;
 use crate::primitives::ray::Ray;
-use crate::primitives::vec3::Vec3;
+use crate::primitives::vec3::{cross, dot, unit_vector, Vec3};
 
 pub struct Quad {
     q: Point3,
@@ -12,18 +12,24 @@ pub struct Quad {
     v: Vec3,
     mat: Box<dyn Material>,
     bbox: Aabb,
+    normal: Vec3,
+    d: f64,
 }
 
 impl Quad {
     pub fn new(q: Point3, u: Vec3, v: Vec3, mat: Box<dyn Material>) -> Self {
         let bbox_diagonal1 = Aabb::new_from_points(&q, &(q + u + v));
         let bbox_diagonal2 = Aabb::new_from_points(&(q + u), &(q + v));
+        let n = cross(&u, &v);
+        let normal = unit_vector(n);
         Self {
             q,
             u,
             v,
             mat,
             bbox: Aabb::new_from_aabb(&bbox_diagonal1, &bbox_diagonal2),
+            normal,
+            d: dot(&normal, &q),
         }
     }
 }
@@ -34,6 +40,18 @@ impl Hittable for Quad {
     }
 
     fn hit(&self, r: &Ray, ray_t: &Interval) -> Option<HitRecord<'_>> {
-        None
+        let denom = dot(&self.normal, r.direction());
+
+        if denom.abs() < 1.0e-8 {
+            None
+        } else {
+            let t = (self.d - dot(&self.normal, r.origin())) / denom;
+            if !ray_t.contains(t) {
+                None
+            } else {
+                let rec = HitRecord::new(t, r, self.normal, self.mat.as_ref(), 0.0, 0.0);
+                Some(rec)
+            }
+        }
     }
 }
