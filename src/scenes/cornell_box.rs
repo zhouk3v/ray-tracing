2cb88c02da0ea3053a9a2eeda43761dbe96c97cb
Kevin Zhou <zhoukev@hotmail.com>
2026-08-07 15:39:26 -0400

    Use BVHNode for cornell box

--- src/scenes/cornell_box.rs
+++ src/scenes/cornell_box.rs
@@ -1,4 +1,5 @@
 use crate::camera::{Camera, CameraFocus, CameraPerformance, CameraPosition, ImageDimensions};
+use crate::hittables::bvh_node::BVHNode;
 use crate::hittables::hittable_list::HittableList;
 use crate::hittables::quad::Quad;
 use crate::materials::diffuse_light::DiffuseLight;
@@ -8,50 +9,53 @@ use crate::primitives::point3::Point3;
 use crate::primitives::vec3::Vec3;
 
 pub fn cornell_box() {
-    let mut world = HittableList::new();
+    let mut objects = HittableList::new();
 
     let green = Box::new(Lambertian::new(Color::new(0.12, 0.45, 0.15)));
-    world.add(Box::new(Quad::new(
+    objects.add(Box::new(Quad::new(
         Point3::new(555.0, 0.0, 0.0),
         Vec3::new(0.0, 555.0, 0.0),
         Vec3::new(0.0, 0.0, 555.0),
         green,
     )));
     let red = Box::new(Lambertian::new(Color::new(0.65, 0.05, 0.05)));
-    world.add(Box::new(Quad::new(
+    objects.add(Box::new(Quad::new(
         Point3::new(0.0, 0.0, 0.0),
         Vec3::new(0.0, 555.0, 0.0),
         Vec3::new(0.0, 0.0, 555.0),
         red,
     )));
     let light = Box::new(DiffuseLight::new_from_color(Color::new(15.0, 15.0, 15.0)));
-    world.add(Box::new(Quad::new(
+    objects.add(Box::new(Quad::new(
         Point3::new(343.0, 554.0, 332.0),
         Vec3::new(-130.0, 0.0, 0.0),
         Vec3::new(0.0, 0.0, -105.0),
         light,
     )));
     let white = Box::new(Lambertian::new(Color::new(0.73, 0.73, 0.73)));
-    world.add(Box::new(Quad::new(
+    objects.add(Box::new(Quad::new(
         Point3::new(0.0, 0.0, 0.0),
         Vec3::new(555.0, 0.0, 0.0),
         Vec3::new(0.0, 0.0, 555.0),
         white,
     )));
     let white = Box::new(Lambertian::new(Color::new(0.73, 0.73, 0.73)));
-    world.add(Box::new(Quad::new(
+    objects.add(Box::new(Quad::new(
         Point3::new(555.0, 555.0, 555.0),
         Vec3::new(-555.0, 0.0, 0.0),
         Vec3::new(0.0, 0.0, -555.0),
         white,
     )));
     let white = Box::new(Lambertian::new(Color::new(0.73, 0.73, 0.73)));
-    world.add(Box::new(Quad::new(
+    objects.add(Box::new(Quad::new(
         Point3::new(0.0, 0.0, 555.0),
         Vec3::new(555.0, 0.0, 0.0),
         Vec3::new(0.0, 555.0, 0.0),
         white,
     )));
+
+    let mut world = HittableList::new();
+    world.add(Box::new(BVHNode::new(objects)));
 
     let image_dim = ImageDimensions::new(1.0, 600.0);
 
