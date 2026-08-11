20f05a82ece008687bb797079e8a1e9ce4fc22ba
Kevin Zhou <zhoukev@hotmail.com>
2026-08-11 15:43:45 -0400

    Use generics for checker texture

--- src/textures/checker_texture.rs
+++ src/textures/checker_texture.rs
@@ -3,31 +3,33 @@ use super::texture::Texture;
 use crate::primitives::color::Color;
 use crate::primitives::point3::Point3;
 
-pub struct CheckerTexture {
+pub struct CheckerTexture<T: Texture, U: Texture> {
     inv_scale: f64,
-    even: Box<dyn Texture>,
-    odd: Box<dyn Texture>,
+    even: T,
+    odd: U,
 }
 
-impl CheckerTexture {
-    pub fn new(scale: f64, even: Box<dyn Texture>, odd: Box<dyn Texture>) -> Self {
+impl<T: Texture, U: Texture> CheckerTexture<T, U> {
+    pub fn new(scale: f64, even: T, odd: U) -> Self {
         Self {
             inv_scale: 1.0 / scale,
             even,
             odd,
         }
     }
+}
 
+impl CheckerTexture<SolidColor, SolidColor> {
     pub fn new_with_color(scale: f64, c1: Color, c2: Color) -> Self {
         Self {
             inv_scale: 1.0 / scale,
-            even: Box::new(SolidColor::new(c1)),
-            odd: Box::new(SolidColor::new(c2)),
+            even: SolidColor::new(c1),
+            odd: SolidColor::new(c2),
         }
     }
 }
 
-impl Texture for CheckerTexture {
+impl<T: Texture, U: Texture> Texture for CheckerTexture<T, U> {
     fn value(&self, u: f64, v: f64, p: &Point3) -> Color {
         let x_integer = (self.inv_scale * p.x()).floor() as i32;
         let y_integer = (self.inv_scale * p.y()).floor() as i32;
